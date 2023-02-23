1e725ec9e26ff61a728f483e95f59c554d138d3a
Ryan Staples <42743016+Srayman@users.noreply.github.com>
2023-02-23 16:14:00 -0500

    enable eslint no-inferrable-types

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,9 @@ module.exports = {
         project: './tsconfig.json',
     },
     extends: "@keetapay/eslint-config-typescript",
+    rules: {
+		'@typescript-eslint/no-inferrable-types': ['error', {
+			'ignoreParameters': true
+		}],
+	},
 }
