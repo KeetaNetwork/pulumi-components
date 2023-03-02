46ad585908e1878d7d266b9bb51bb681c796402d
Roy Keene <rkeene@keeta.com>
2023-03-01 17:21:40 -0800

    Add policy management tools

---
+++ src/policy.ts
@@ -0,0 +1,238 @@
+import * as pulumi from '@pulumi/pulumi';
+import * as gcpLegacy from '@pulumi/gcp';
+
+export interface PolicyDataBinding {
+	type: 'bucket' | 'project' | 'keyring' | 'key' | 'config';
+	name: pulumi.Input<string>;
+	id: string;
+	policyData: pulumi.Output<string>;
+	addMembers(role: string, members: pulumi.Input<string>[]): void;
+}
+
+class PolicyBindingBase {
+	private members: { [role: string]: pulumi.Input<string>[] } = {};
+
+	addMembers(role: string, members: pulumi.Input<string>[]) {
+		if (!(role in this.members)) {
+			this.members[role] = [];
+		}
+		const roleMembers = this.members[role];
+		roleMembers.push(...members);
+	}
+
+	get policyData() {
+		return(pulumi.jsonStringify({
+			bindings: Object.entries(this.members).map(function([role, members]) {
+				return({ role, members });
+			})
+		}));
+	}
+}
+
+export class BucketPolicyBinding extends PolicyBindingBase implements PolicyDataBinding {
+	readonly type = 'bucket';
+	readonly id: string;
+	readonly name: pulumi.Input<string>;
+	readonly bucket?: gcpLegacy.storage.Bucket;
+	private static cachedBuckets: { [bucketName: string]: BucketPolicyBinding } = {};
+	private static cachedBucketResources: { [resourceName: string]: BucketPolicyBinding } = {};
+
+	constructor(resourceId: string, resourceName: pulumi.Input<string>, bucket?: gcpLegacy.storage.Bucket) {
+		super();
+
+		this.id = resourceId;
+		this.name = resourceName;
+		this.bucket = bucket;
+	}
+
+	static fromBucketName(bucketName: string) {
+		if (bucketName in this.cachedBuckets) {
+			return(this.cachedBuckets[bucketName]);
+		}
+
+		this.cachedBuckets[bucketName] = new BucketPolicyBinding(bucketName, bucketName);
+
+		return(this.cachedBuckets[bucketName]);
+	}
+
+	static createBucket(...args: ConstructorParameters<typeof gcpLegacy.storage.Bucket>) {
+		const resourceName = args[0];
+		if (resourceName in this.cachedBucketResources) {
+			return(this.cachedBucketResources[resourceName]);
+		}
+
+		const bucket = new gcpLegacy.storage.Bucket(...args);
+		const retval = new BucketPolicyBinding(resourceName, bucket.name, bucket);
+
+		this.cachedBucketResources[resourceName] = retval;
+
+		return(this.cachedBucketResources[resourceName]);
+	}
+}
+
+export class ProjectPolicyBinding extends PolicyBindingBase implements PolicyDataBinding {
+	readonly type = 'project';
+	readonly id: string;
+	readonly name: string;
+	private static cachedProjects: { [projectName: string]: ProjectPolicyBinding } = {};
+
+	constructor(projectName: string) {
+		super();
+
+		this.id = projectName;
+		this.name = projectName;
+	}
+
+	static fromProjectName(projectName: string) {
+		if (projectName in this.cachedProjects) {
+			return(this.cachedProjects[projectName]);
+		}
+
+		this.cachedProjects[projectName] = new ProjectPolicyBinding(projectName);
+
+		return(this.cachedProjects[projectName]);
+	}
+
+}
+
+export class KeyRingPolicyBinding extends PolicyBindingBase implements PolicyDataBinding {
+	readonly type = 'keyring';
+	readonly id: string;
+	readonly name: pulumi.Input<string>;
+	readonly keyring?: gcpLegacy.kms.KeyRing;
+	private static cachedKeyRingsByID: { [keyringID: string]: KeyRingPolicyBinding } = {};
+
+	constructor(resourceId: string, resourceName: pulumi.Input<string>, keyring?: gcpLegacy.kms.KeyRing) {
+		super();
+
+		this.id = resourceId;
+		this.name = resourceName;
+		this.keyring = keyring;
+	}
+
+	static createKeyRing(...args: ConstructorParameters<typeof gcpLegacy.kms.KeyRing>) {
+		const resourceName = args[0];
+		if (resourceName in this.cachedKeyRingsByID) {
+			return(this.cachedKeyRingsByID[resourceName]);
+		}
+
+		const keyring = new gcpLegacy.kms.KeyRing(...args);
+		const retval = new KeyRingPolicyBinding(resourceName, keyring.name, keyring);
+
+		this.cachedKeyRingsByID[resourceName] = retval;
+
+		return(this.cachedKeyRingsByID[resourceName]);
+	}
+}
+
+export class KeyPolicyBinding extends PolicyBindingBase implements PolicyDataBinding {
+	readonly type = 'key';
+	readonly id: string;
+	readonly name: pulumi.Input<string>;
+	readonly key: gcpLegacy.kms.CryptoKey;
+	private static cachedKeysByID: { [keyringID: string]: KeyPolicyBinding } = {};
+
+	constructor(resourceId: string, resourceName: pulumi.Input<string>, key: gcpLegacy.kms.CryptoKey) {
+		super();
+
+		this.id = resourceId;
+		this.name = resourceName;
+		this.key = key;
+	}
+
+	static createKey(...args: ConstructorParameters<typeof gcpLegacy.kms.CryptoKey>) {
+		const resourceName = args[0];
+		if (resourceName in this.cachedKeysByID) {
+			return(this.cachedKeysByID[resourceName]);
+		}
+
+		const key = new gcpLegacy.kms.CryptoKey(...args);
+		const retval = new KeyPolicyBinding(resourceName, key.name, key);
+
+		this.cachedKeysByID[resourceName] = retval;
+
+		return(this.cachedKeysByID[resourceName]);
+	}
+}
+
+export class ConfigPolicyBinding extends PolicyBindingBase implements PolicyDataBinding {
+	readonly type = 'config';
+	readonly id: string;
+	readonly name: pulumi.Input<string>;
+	readonly config: gcpLegacy.runtimeconfig.Config;
+
+	constructor(resourceId: string, resourceName: pulumi.Input<string>, config: gcpLegacy.runtimeconfig.Config) {
+		super();
+
+		this.id = resourceId;
+		this.name = resourceName;
+		this.config = config;
+	}
+
+	static createConfig(...args: ConstructorParameters<typeof gcpLegacy.runtimeconfig.Config>) {
+		const resourceName = args[0];
+		const config = new gcpLegacy.runtimeconfig.Config(...args);
+		const retval = new ConfigPolicyBinding(resourceName, config.name, config);
+		return(retval);
+	}
+}
+
+export async function applyBindings(bindings: PolicyDataBinding[], allowProject = false) {
+	const seenIDs = new Set<string>();
+	for (const binding of bindings) {
+		const { type, id, name, policyData } = binding;
+		if (seenIDs.has(id)) {
+			continue;
+		}
+		seenIDs.add(id);
+
+		const policyResourceName = `policy-${type}-${utils.hash(id)}`;
+
+		switch (type) {
+			case 'bucket':
+				new gcpLegacy.storage.BucketIAMPolicy(policyResourceName, {
+					bucket: name,
+					policyData: policyData
+				});
+				break;
+			case 'project':
+				if (!allowProject) {
+					throw(new Error('Tried to set Project policy (dangerous!)'));
+				}
+
+				new gcpLegacy.projects.IAMPolicy(policyResourceName, {
+					project: name,
+					policyData: policyData
+				}, {
+					protect: true
+				});
+				break;
+			case 'keyring':
+				new gcpLegacy.kms.KeyRingIAMPolicy(policyResourceName, {
+					keyRingId: pulumi.interpolate`${state.project}/${primaryDeploymentRegion}/${name}`,
+					policyData: policyData
+				});
+				break;
+			case 'key':
+				{
+					if (!(binding instanceof policy.KeyPolicyBinding)) {
+						throw(new Error('internal error: mismatch between type and object'));
+					}
+
+					const keyRingName = utils.tail(binding.key.keyRing);
+
+					new gcpLegacy.kms.CryptoKeyIAMPolicy(policyResourceName, {
+						cryptoKeyId: pulumi.interpolate`${state.project}/${primaryDeploymentRegion}/${keyRingName}/${name}`,
+						policyData: policyData
+					});
+				}
+				break;
+			case 'config':
+				new gcpLegacy.runtimeconfig.ConfigIamPolicy(policyResourceName, {
+					config: name,
+					policyData: policyData
+				});
+				break;
+		}
+	}
+}
