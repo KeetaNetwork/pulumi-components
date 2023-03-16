6553f7dd8f9b3788ec0f0d80caf24e7a067756b0
Roy Keene <rkeene@keeta.com>
2023-03-15 23:32:47 -0700

    tarball: Create a tarball Asset from a directory without it being in version control

--- src/packages/tarball.ts
+++ src/packages/tarball.ts
@@ -4,10 +4,84 @@ import * as os from 'os';
 import * as child_process from 'child_process';
 import * as path from 'path';
 import * as process from 'process';
+import * as crypto from 'crypto';
 
 import * as utils from '../utils';
 
-function createTarballFromGit(directory: string, commitID: string = 'HEAD'): { commit: string, file: string } {
+/**
+ * Create a tarball from a directory, caching it in the home directory
+ * and returning the path to the tarball.  If a cacheID is provided, it
+ * will be used to cache the tarball.
+ */
+function createTarballFromDir(directory: string, cacheID?: string): { file: string, uniqueID: string, shouldClean: boolean } {
+	let shouldClean = false;
+	if (cacheID === undefined) {
+		cacheID = crypto.randomUUID();
+
+		shouldClean = true;
+	}
+
+	const tarballDir = path.join(os.homedir(), '.cache', 'pulumi-tarballs');
+	fs.mkdirSync(tarballDir, {
+		recursive: true
+	});
+
+	const canonicalDir = fs.realpathSync(directory);
+	const dirHash = utils.hash(canonicalDir, 32);
+	cacheID = utils.hash(cacheID, 32);
+
+	const uniqueID = `${dirHash}-${cacheID}`;
+
+	const tarballPath = path.join(tarballDir, `${uniqueID}.tar.gz`);
+	const tarballTmpPath = `${tarballPath}.new`;
+
+	if (fs.existsSync(tarballPath)) {
+		return({
+			uniqueID: uniqueID,
+			file: tarballPath,
+
+			/* If we didn't create this tarball, we shouldn't clean it up */
+			shouldClean: false
+		});
+	}
+
+	/**
+	 * Create the tarball
+	 */
+	const createResults = child_process.spawnSync('tar', ['-C', directory, '-zcvf', tarballTmpPath, '.'], {
+		env: {
+			...process.env,
+			LC_ALL: 'C',
+			LANG: 'C'
+		}
+	});
+	if (createResults.status !== 0) {
+		throw new Error(`tar failed: ${createResults.stderr.toString()}`);
+	}
+
+	/**
+	 * Verify that the created tarball is valid
+	 */
+	const checkResults = child_process.spawnSync('tar', ['-ztf', tarballTmpPath]);
+	if (checkResults.status !== 0) {
+		throw new Error(`tar failed: ${checkResults.stderr.toString()}`);
+	}
+
+	fs.renameSync(tarballTmpPath, tarballPath);
+
+	return({
+		uniqueID: uniqueID,
+		file: tarballPath,
+		shouldClean: shouldClean
+	});
+}
+
+/**
+ * Create a tarball from a git commit of a directory.  This will
+ * cache the tarball in the home directory and return the path to
+ * the tarball.
+ */
+function createTarballFromGit(directory: string, commitID: string = 'HEAD'): { commit: string, file: string, uniqueID: string } {
 	let commit: string;
 	if (commitID === 'HEAD') {
 		commit = child_process.execSync(`git log --max-count=1 --format=%H ${commitID} .`, {
@@ -36,12 +110,16 @@ function createTarballFromGit(directory: string, commitID: string = 'HEAD'): { c
 
 	const canonicalDir = fs.realpathSync(directory);
 	const dirHash = utils.hash(canonicalDir, 32);
-	const tarballPath = path.join(tarballDir, `${dirHash}-${commit}.tar.gz`);
+
+	const uniqueID = `${dirHash}-${commit}`;
+
+	const tarballPath = path.join(tarballDir, `${uniqueID}.tar.gz`);
 	const tarballTmpPath = `${tarballPath}.new`;
 
 	if (fs.existsSync(tarballPath)) {
 		return({
 			commit: commit,
+			uniqueID: uniqueID,
 			file: tarballPath
 		});
 	}
@@ -80,6 +158,7 @@ function createTarballFromGit(directory: string, commitID: string = 'HEAD'): { c
 
 	return({
 		commit: commit,
+		uniqueID: uniqueID,
 		file: tarballPath
 	});
 }
@@ -88,12 +167,76 @@ function createTarballFromGit(directory: string, commitID: string = 'HEAD'): { c
  * Create a tarball from a Git checkout of a directory
  */
 export class GitTarballArchive extends pulumi.asset.FileAsset {
+	/**
+	 * Fully resolved commit
+	 */
 	readonly commit: string;
 
+	/**
+	 * Unique ID for the tarball, based on the commit and the directory
+	 * it was created from
+	 */
+	readonly uniqueID: string;
+
 	constructor(dir: string, commitID?: string) {
-		const { commit, file } = createTarballFromGit(dir, commitID);
+		const { commit, file, uniqueID } = createTarballFromGit(dir, commitID);
 
 		super(file);
 		this.commit = commit;
+		this.uniqueID = uniqueID;
+	}
+
+	clean() {
+		/* Do nothing, but have the same interface as DirTarballArchive */
 	}
 }
+
+/**
+ * Create a tarball from a directory
+ */
+export class DirTarballArchive extends pulumi.asset.FileAsset {
+	/**
+	 * Unique ID for the tarball, based on the cacheID and the directory
+	 * it was created from
+	 */
+	readonly uniqueID: string;
+
+	/**
+	 * Should the generated archive be cleaned up because there is no
+	 * value in caching it because no cache ID was supplied ?
+	 */
+	private shouldClean: boolean;
+
+	/**
+	 * Path of generated tarball
+	 */
+	private _path: string;
+
+	constructor(dir: string, cacheID?: string) {
+		const { file, uniqueID, shouldClean } = createTarballFromDir(dir, cacheID);
+
+		super(file);
+		this.uniqueID = uniqueID;
+		this.shouldClean = shouldClean;
+		this._path = file;
+	}
+
+	/**
+	 * Clean up the generated tarball if it is not cachable
+	 */
+	clean() {
+		if (!this.shouldClean) {
+			return;
+		}
+
+		try {
+			fs.unlinkSync(this._path);
+		} catch {
+			/* Ignore errors */
+		}
+
+		this.shouldClean = false;
+	}
+}
+
+export default GitTarballArchive;
