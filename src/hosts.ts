b118cf39a1d156638a295e95db5d5ddaa8cc3edd
Ezra Ripps <eripps@keeta.com>
2023-04-25 20:09:06 -0400

    Add hosts file

---
+++ src/hosts.ts
@@ -0,0 +1,31 @@
+import { execSync } from 'child_process';
+
+const foundHostCache: { [key: string]: string } = {};
+
+function getAndAddDomainFromHostTLS(url: string) {
+	const parsed = new URL(url);
+
+	const starttls = parsed.protocol.split(':')[0];
+
+	const foundDomainResp = execSync(`echo '' | openssl s_client -connect ${parsed.host} -starttls ${starttls} 2>/dev/null | openssl x509 -ext subjectAltName -noout | awk '/DNS:/{ sub(/[[:space:]]*DNS:/, ""); print; }'`);
+	const foundDomain = foundDomainResp.toString().trim();
+
+	execSync(`echo '${parsed.hostname} ${foundDomain} # Added by Jester' | tee -a /etc/hosts`);
+
+	return foundDomain;
+}
+
+export function addSubjectAlternativeToHosts(url: string) {
+	const parsed = new URL(url);
+	const { hostname, protocol } = parsed;
+
+	const updateKey = `${protocol}${hostname}`;
+
+	if (foundHostCache[updateKey] === undefined) {
+		foundHostCache[updateKey] = getAndAddDomainFromHostTLS(url);
+	}
+
+	parsed.host = foundHostCache[updateKey];
+
+	return parsed.toString();
+}
