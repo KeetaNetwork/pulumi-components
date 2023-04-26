b60eb0eff2c9a04478a5d3e53371b43a7fc1ba21
Ezra Ripps <eripps@keeta.com>
2023-04-25 20:15:38 -0400

    add retry logic

--- src/hosts.ts
+++ src/hosts.ts
@@ -2,21 +2,33 @@ import { execSync } from 'child_process';
 
 const foundHostCache: { [key: string]: string } = {};
 
+const maxAttempts = 5;
+
 function getAndAddDomainFromHostTLS(url: string) {
 	const parsed = new URL(url);
 
 	const starttls = parsed.protocol.split(':')[0];
 
-	const foundDomainResp = execSync(`echo '' | openssl s_client -connect ${parsed.host} -starttls ${starttls} 2>/dev/null | openssl x509 -ext subjectAltName -noout | awk '/DNS:/{ sub(/[[:space:]]*DNS:/, ""); print; }'`);
-	const foundDomain = foundDomainResp.toString().trim();
+	let error;
 
-	if (!foundDomain || foundDomain === '') {
-		throw new Error('Could not find domain from TLS certificate');
+	for (let attempt = 0; attempt < maxAttempts; attempt++) {
+		try {
+			const foundDomainResp = execSync(`echo '' | openssl s_client -connect ${parsed.host} -starttls ${starttls} 2>/dev/null | openssl x509 -ext subjectAltName -noout | awk '/DNS:/{ sub(/[[:space:]]*DNS:/, ""); print; }'`);
+			const foundDomain = foundDomainResp.toString().trim();
+
+			if (!foundDomain || foundDomain === '') {
+				throw new Error('Could not find domain from TLS certificate');
+			}
+
+			execSync(`echo '${parsed.hostname} ${foundDomain} # Added Automatically by @keetapay/pulumi-components' | tee -a /etc/hosts`);
+
+			return foundDomain;
+		} catch (e) {
+			error = e;
+		}
 	}
 
-	execSync(`echo '${parsed.hostname} ${foundDomain} # Added Automatically by @keetapay/pulumi-components' | tee -a /etc/hosts`);
-
-	return foundDomain;
+	throw error;
 }
 
 export function addSubjectAlternativeToHosts(url: string) {
